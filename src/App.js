f8aecd7b957ce179a3bd99136bd3b889c8da4487
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-07-17 17:22:16 +1000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1MZx4gn6sZB83dm16fgaGG-wLw6mId_o",
+  authDomain: "cafe-web-app-d741e.firebaseapp.com",
+  projectId: "cafe-web-app-d741e",
+  storageBucket: "cafe-web-app-d741e.appspot.com",
+  messagingSenderId: "439419802863",
+  appId: "1:439419802863:web:3b0f0830d02de8b3a3771a",
+  measurementId: "G-TSK6NFDJNB"
+}
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
